e8aa675dfc344f66b7fb135659ecd2e89b247499
Glitch18 <atharvakn@gmail.com>
2020-12-24 11:22:50 +0530

    ADD: Active/Passive detection

--- TXProcessor.py
+++ TXProcessor.py
@@ -1,20 +1,38 @@
 import spacy
 import nltk
+import regex as re
 from nltk.tokenize import sent_tokenize
+from spacy.matcher import Matcher
 
 class TX_Processor:
 
     def __init__(self, in_text):
         nltk.download('punkt')
-        self.text = in_text #input text given to an instance of TXP
-        self.lines = sent_tokenize(self.text)
-        self.sp_model = spacy.load('en_core_web_sm') #model for POS tagging
+        #input text given to an instance of TXP:
+        self.text = re.sub(r'([a-z]).([A-Z])',r'\1. \2',in_text)
+
+        self.lines = sent_tokenize(self.text) #Sentence tokenize
+        self.model = spacy.load('en_core_web_sm') #model for POS tagging
+        self.matcher = Matcher(self.model.vocab)
 
     def print_lines(self):
-        print(self.lines)
+        for line in self.lines:
+            print(line)
 
     def pos_tag(self):
         for line in self.lines:
-            sen = self.sp_model(line)
+            sen = self.model(line)
             for word in sen:
                 print(f'{word.text:{12}} {word.pos_:{10}} {word.tag_:{8}} {spacy.explain(word.tag_)}')
+
+    def isActive(self,sentence):
+        line = self.model(sentence)
+        passive_rule = [{'DEP': 'nsubjpass'}, {'DEP': 'aux', 'OP': '*'},\
+                        {'DEP': 'auxpass'}, {'TAG': 'VBN'}]
+        self.matcher.add('Passive', None, passive_rule)
+        matches = self.matcher(line)
+
+        if matches:
+            return 0
+        else:
+            return 1
