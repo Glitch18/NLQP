0315d08b8b10d72f3ec05140cb9650d7cae2f192
Glitch18 <atharvakn@gmail.com>
2020-12-29 17:18:44 +0530

    Bug Fixes

--- TXProcessor.py
+++ TXProcessor.py
@@ -4,8 +4,25 @@ import regex as re
 from nltk.tokenize import sent_tokenize
 from spacy.matcher import Matcher
 
+class bcolors:
+    #IGNORE THIS CLASS
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class Sentence:
     def __init__(self,id):
+        #TODO - who -> subject
+        #WHERE -> place
+        #WHEN -> time/date
+        #Use nltk or something else to identify places and time
+        self.subject = ""
+        self.direct_obj = ""
         self.id = id
         self.indirect_obj = list()
         self.verbs = list()
@@ -14,10 +31,10 @@ class Sentence:
         self.nouns = list()
 
     def set_subject(self,subj):
-        self.subject = subj
+        self.subject += subj
 
     def set_object(self,obj):
-        self.direct_obj = obj
+        self.direct_obj += obj
 
     def add_nouns(self,nun):
         self.nouns.append(nun)
@@ -40,13 +57,33 @@ class Sentence:
     def add_adverb(self,obj):
         if(not isinstance(obj,str)):
             raise NameError("Invaid Argument Type")
+
         self.adverbs.append(obj)
+
+    def show_details(self):
+        #self.id = id
+        #self.indirect_obj = list()
+        #self.verbs = list()
+        #self.adjectives = list()
+        #self.adverbs = list()
+        #self.nouns = list()
+        print("From this line the following data was extracted: ")
+        print(f"Main subject: {self.subject}")
+        print(f"line id: {self.id}")
+        print(f"indirect objects: {self.indirect_obj}")
+        print(f"verbs: {self.verbs}")
+        print(f"adjectives: {self.adjectives}")
+        print(f"adverbs: {self.adverbs}")
+        print(f"other nouns: {self.nouns}")
+        print("\n")
 
 
 class TX_Processor:
     def __init__(self, in_text):
         nltk.download('punkt')
         #input text given to an instance of TXP:
+        #TODO - JSON files for storage | Cache database
+        #TODO - A dict for storage
         self.text = re.sub(r'([a-z])\.([A-Z])',r'\1. \2',in_text)
         self.lines = sent_tokenize(self.text) #Sentence tokenize
         self.model = spacy.load('en_core_web_sm') #model for POS tagging
@@ -57,12 +94,12 @@ class TX_Processor:
 
     def extract(self):
         for line in self.lines:
+            print(f"{bcolors.OKGREEN}{bcolors.BOLD}{line}{bcolors.ENDC}")
             tags = self.pos_tag(line)
             entry = Sentence(self.count)
             self.count += 1
             for tag in tags:
                 if(tag[1]=="PROPN"):
-                    print(tag[1])
                     entry.set_subject(tag[0])
                 elif(tag[1]=='VERB'):
                     entry.add_verb(tag[0])
@@ -72,6 +109,7 @@ class TX_Processor:
                     entry.add_adverb(tag[0])
                 elif(tag[1]=="ADJ"):
                     entry.add_adj(tag[0])
+            entry.show_details()
             self.data.append(entry)
 
 
@@ -84,7 +122,7 @@ class TX_Processor:
         tags = list()
         for word in sen:
             #print(f'{word.text:{12}} {word.pos_:{10}}')
-            tags.append((word,word.pos_))
+            tags.append((word.text,word.pos_))
         return tags
 
     def isActive(self,sentence):
